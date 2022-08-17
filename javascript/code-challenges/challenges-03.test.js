898426ab6cb55942256b7eef248a2285b201f736
Keelen Fisher <keelencarrera@gmail.com>
2022-08-17 16:19:46 -0500

    working again

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -7,7 +7,11 @@ Write a function called addTwo that takes in an array and adds two to every valu
 ------------------------------------------------------------------------------------------------ */
 
 const addTwo = (arr) => {
-  
+  let newArr = [];
+  for (let i = 0; i < arr.length; i + 2){
+    newArr.push([i]);
+  }
+  return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
