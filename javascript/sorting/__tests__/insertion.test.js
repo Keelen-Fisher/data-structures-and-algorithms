75df107ec116da93c9cf8ddd2f9d6c057c21ba96
Keelen Fisher <keelencarrera@gmail.com>
2022-10-25 17:50:18 -0500

    created test for insertion and merge

--- javascript/sorting/__tests__/insertion.test.js
+++ javascript/sorting/__tests__/insertion.test.js
@@ -1,11 +1,12 @@
 'use strict';
 
-const insert = require('../insertion/index.js');
+const insertionSort = require('../insertion/index.js');
 
 describe('Insertion Sort Test', () => {
   it('Can Loop though an array and sort', () => {
+    let arr = [8, 4, 23, 42, 16, 15];
+    let result = insertionSort(arr);
+    expect(result).toEqual([4, 8, 15, 16, 23, 42]);
   });
 
-  it('can insert and rearrange the array given.', () => {
-  });
 });
